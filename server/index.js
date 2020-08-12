1b4be23b9197dcaf1e51a94cdd3bd6caff652be0
adi <adi@benoz.net>
2020-08-12 17:03:33 +0300

    added a private folder, only one with cookie access it

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser')
 const redisClient = require('./redisConnector');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const shortid = require('shortid');
+var cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 createAdminUser();
 
@@ -17,20 +19,11 @@ app.listen(PORT, () => {
 app.post('/register', (req, res) => {
     try {
         let email = req.body.email;
-        let userDetails = {
-            name: req.body.fullname,
-            email,
-            password: req.body.password
-        }
-        let user = {
-            userDetails,
-            cart: [],
-            purchases: [],
-            loginActivity: []
-        }
-        let date = new Date(Date.now());
-        user.loginActivity.push(date.toString());
+        let password = req.body.password;
+        let name = req.body.fullname;
+        let user = generateUser(email, name, password);
         redisClient.hmset('users', email, (JSON.stringify(user)));
+
         return res.sendStatus(200);
     } catch (e) {
         return res.sendStatus(500);
@@ -40,34 +33,58 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    let userCheck = redisClient.hget('users', email, function (err, user) {
+
+    let rememberMe = req.body['rememberMe'];
+    redisClient.hget('users', email, function (err, user) {
         if(!user) return res.send("user doesn't exist");
         let userJson = JSON.parse(user);
         if (userJson.userDetails.password != password) return res.send("user name or password incorrect");
         // in case user logged in correctly
         let sid = shortid.generate();
-        res.cookie('sid', sid);
+        if (rememberMe) {
+            res.cookie('sid', sid);
+        } else {
+            // set timeout 30 min
+            res.cookie('sid', sid, { maxAge: 1800000 });
+        }
+
+        if(userJson.email == "admin"){
+            res.cookie('admin', 'admin');
+        }
         //users[sid] = {id:sid};
         return res.sendStatus(200);
     });
 });
 
+app.get('/private/*', (req, res, next)=>{
+    if(req.cookies.sid){
+        next();
+    } else {
+        res.redirect('/error.html');
+    }
+})
+
 app.use(express.static('./client/src'));
 
 function createAdminUser(){
     let email = "admin";
-        let userDetails = {
-            name: "admin",
-            email,
-            password: "admin"
-        }
-        let user = {
-            userDetails,
-            cart: [],
-            purchases: [],
-            loginActivity: []
-        }
-        let date = new Date(Date.now());
-        user.loginActivity.push(date.toString());
-        redisClient.hmset('users', email, (JSON.stringify(user)));
+    let user = generateUser(email, "admin", "admin");
+    redisClient.hmset('users', email, (JSON.stringify(user)));
+}
+
+function generateUser(email, name, password){
+    let userDetails = {
+        name,
+        email,
+        password
+    }
+    let user = {
+        userDetails,
+        cart: [],
+        purchases: [],
+        loginActivity: []
+    }
+    let date = new Date(Date.now());
+    user.loginActivity.push(date.toString());
+    return user;
 }
